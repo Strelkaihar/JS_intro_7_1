8be665187dc121369505e3d716ca156783526801
Strelkaihar <ihar.strelka@gmail.com>
2023-11-07 10:06:12 -0600

    Cypress project 1

--- cypress-framework/cypress/support/commands.js
+++ cypress-framework/cypress/support/commands.js
@@ -28,6 +28,23 @@ Cypress.Commands.add("login", (userName, password) => {
 Cypress.Commands.add("selectDropDownOption", (selector, value) => {
     cy.get(selector).select(value);
 })
+/**
+ * 	IMPORTANT NOTE
+ * 	
+ * 	When you define a custom command in Cypress and use prevSubject: true, or 'element' or 'window' or 'document', 
+ * the command will automatically yield the subject.
+ * 	
+ * 	When you set prevSubject: true, Your custom command functions acts like a .then() callback, giving you direct access 
+ * to subject so you can interact with it like jQuery Object
+ * 	
+ * 	
+ * 	Key Point Here:
+ * 	
+ * 	1. prevSubhect ensures that your command is a child command 
+ *  that will operate on the subject yielded by the previous command in the chain.
+ * 	2. The function you provide Cypress.Commands.add with prevSubject: true 
+ *  works like .then() in that block and lets you directly interact with the yielded subject.
+ */
 
 // /**
 //  * 
@@ -48,10 +65,17 @@ Cypress.Commands.add("logText",{prevSubject: true}, (subject) => {
     cy.log(`My text is ${text}`)
 })
 
+Cypress.Commands.add("haveText",{prevSubject: 'element'}, (subject, expectedText) => {
+    cy.wrap(subject).should('have.text', expectedText)
+})
+Cypress.Commands.add("assertAttribute ",{prevSubject: 'element'}, (subject, expectedAttr, value) => {
+    cy.wrap(subject).should('have.attr', expectedAttr, value)
+})
+
 //
 // -- This is a dual command --
 // Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
+
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
